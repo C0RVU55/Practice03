920c3ce8148d1bd4dd9291ac9f871e219a9e494d
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-17 12:37:47 +0900

    Practice03

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		//문제1
+		
+		//결과 : while문과 for문 둘 다 같은 결과임.
+		
+		System.out.println("while문");
+		int index=0;
+		while(index<3) {
+			System.out.println("index="+index);
+			index++;
+		}
+
+	}
+
+}
